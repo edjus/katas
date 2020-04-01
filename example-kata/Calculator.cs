949d08c2702dc263e5482806e640d1e4bafc44f2
unknown <justo.edson@gmail.com>
2020-04-01 17:06:37 -0300

    base project

---
+++ example-kata/Calculator.cs
@@ -0,0 +1,13 @@
+namespace katas.example_kata
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
