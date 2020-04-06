415c1eb5aab9db0b3187f78a1d48669da26ed5e6
edjus <ejusto@fi.uba.ar>
2020-04-06 10:29:14 -0300

    refactor primeFactorsOf

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -9,10 +9,9 @@ namespace katas.prime_factors
         public List<int> primeFactorsOf(int number)
         {
             var primeFactors = new List<int>();
-            if (number == 2)
-                primeFactors.Add(2);
-            if (number == 3)
-                primeFactors.Add(3);
+            if (number > 1)
+                primeFactors.Add(number);
+                
             return primeFactors;
         }
     }
